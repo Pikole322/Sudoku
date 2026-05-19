9e08f6a7ede2eef4556a1864a0360e103da1ba4c
Bartosz Adamczyk <bartek334000@gmail.com>
2026-05-19 22:53:20 +0200

    Dodanie grafik oraz wstępna próba dodania kodu z grafikami/full screen

--- Form1.cs
+++ Form1.cs
@@ -2,12 +2,45 @@ namespace Sudoku
 {
     public partial class Form1 : Form
     {
+        
+
+
         int[,] table;
 
         TextBox selected_textbox;
         public Form1()
         {
+            var panel1 = new Panel
+            {
+                Dock = DockStyle.Fill,
+                BackgroundImageLayout = ImageLayout.Stretch,
+                BackColor = Color.White
+            };
+
+            string imagesDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Images");
+            string imageFile = Path.Combine(imagesDir, "background.png");
+
+            if (File.Exists(imageFile))
+            {
+                try
+                {
+                    panel1.BackgroundImage = Image.FromFile(imageFile);
+                }
+                catch
+                {
+                    panel1.BackgroundImage = null;
+                }
+            }
+            else
+            {
+                panel1.BackgroundImage = null;
+            }
+
             InitializeComponent();
+            this.Controls.Add(panel1);
+            panel1.SendToBack();
+
+            this.WindowState = FormWindowState.Maximized;
 
             table = new int [9, 9]{
                 { 5, 4, 2, 6, 3, 9, 8, 1, 7},
@@ -20,62 +53,80 @@ namespace Sudoku
                 { 1, 3, 5, 2, 8, 4, 6, 7, 9},
                 { 7, 2, 9, 1, 6, 3, 4, 5, 8},
             };
- 
 
 
-            int spacing = 5;
-
-            int box_size = 30;
-            Point offset = new Point(ClientSize.Width / 2 - 9 * box_size / 2 - 2 * spacing / 2, ClientSize.Height / 2 - 9 * box_size / 2 - 2 * spacing / 2);
-
-            int spacing_x = -spacing;
-            int spacing_y = -spacing;
 
             for (int y = 0; y < 9; y++)
             {
-                spacing_y = y % 3 == 0 ? spacing_y + 10 : spacing_y;
-                spacing_x = -spacing;
                 for (int x = 0; x < 9; x++)
                 {
                     textboxes[y, x] = new TextBox();
-                    spacing_x = x % 3 == 0 ? spacing_x + 10 : spacing_x;
-                    //textboxes[y, x]
-                    textboxes[y, x].SetBounds(x * box_size + offset.X + spacing_x, y * box_size + offset.Y + spacing_y, box_size, box_size);
+                    textboxes[y, x].SetBounds(0, 0, 30, 30);
                     textboxes[y, x].Multiline = true;
                     textboxes[y, x].TextAlign = HorizontalAlignment.Center;
                     textboxes[y, x].Font = new Font("Arial", 14, FontStyle.Regular);
                     textboxes[y, x].TextChanged += on_number_changed;
                     textboxes[y, x].Tag = new Point(x, y);
-                 
-                    
                     this.Controls.Add(textboxes[y, x]);
                 }
-            
             }
+
+            LayoutBoard();
+            this.Resize += (s, e) => LayoutBoard();
+        }
+
+        private void LayoutBoard()
+        {
+            float boardProportion = 0.82f;
+            int minSide = Math.Min(ClientSize.Width, ClientSize.Height);
+            int computedBoxSize = Math.Max(24, (int)(minSide * boardProportion / 9f)); 
+
+            int blockGap = Math.Max(8, computedBoxSize / 3);
+
+            int boardWidth = 9 * computedBoxSize + 2 * blockGap;
+            int boardHeight = 9 * computedBoxSize + 2 * blockGap;
+
+            Point offset = new Point(
+                (ClientSize.Width - boardWidth) / 2,
+                (ClientSize.Height - boardHeight) / 2
+            );
+
+            float fontSize = Math.Max(12f, computedBoxSize * 0.55f);
+
+            for (int y = 0; y < 9; y++)
+            {
+                for (int x = 0; x < 9; x++)
+                {
+                    int extraX = (x / 3) * blockGap;
+                    int extraY = (y / 3) * blockGap;
+
+                    int xPos = offset.X + x * computedBoxSize + extraX;
+                    int yPos = offset.Y + y * computedBoxSize + extraY;
+
+                    textboxes[y, x].SetBounds(xPos, yPos, computedBoxSize, computedBoxSize);
+                    textboxes[y, x].Font = new Font(textboxes[y, x].Font.FontFamily, fontSize, textboxes[y, x].Font.Style);
+                }
+            }
+
+            Invalidate(); 
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
-            //Pen purple = new Pen(Color.Purple);
-
-            //textbox.Invalidate();
-            //Graphics g = e.Graphics;
-            //if (selected_textbox != null)
-                //g.FillRectangle(Brushes.Purple, new Rectangle(selected_textbox.Location, selected_textbox.Size));
-            //g.DrawRectangle(purple, new Rectangle((int)l, 0, 30, 30));
-            //Invalidate();
-            //Refresh();
-            //Invalidate();
-
-
         }
 
         private void on_number_changed(object sender, EventArgs e)
         {
             var box = sender as TextBox;
             Point pos = (Point)box.Tag;
-            int num = int.Parse(box.Text); // NALEZY DODAC TRYPARSE LUB ZABLOKOWAC MOZLIWOSC WPISYWANIA NIE CYFER
+
+            if (!int.TryParse(box.Text, out int num))
+            {
+                box.BackColor = Color.White;
+                selected_textbox = box;
+                return;
+            }
 
             if (table[pos.Y, pos.X] != num)
             {
